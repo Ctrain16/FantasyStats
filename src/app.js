a818ec90e13cc6581876cd216205eee9f0d70e02
Cal Trainor <cal16trainor@gmail.com>
2021-07-02 23:04:00 -0300

    Player data is now being posted to MongoDB

--- src/app.js
+++ src/app.js
@@ -1,5 +1,8 @@
 import fetch from 'node-fetch';
+import { MongoClient } from 'mongodb';
 import Player from './player.js';
+import * as dotenv from 'dotenv';
+dotenv.config();
 
 const tryCatchForAsync = async function (promise) {
   try {
@@ -37,9 +40,7 @@ const getPlayers = async function () {
 
 const getPlayersStatsSingleSeason = async function (player, season) {
   const [response, error] = await tryCatchForAsync(
-    fetch(
-      `https://statsapi.web.nhl.com/api/v1/people/${player.id}/stats?stats=yearByYear`
-    )
+    fetch(`https://statsapi.web.nhl.com/${player.link}/stats?stats=yearByYear`)
   );
   if (error) return;
 
@@ -49,18 +50,31 @@ const getPlayersStatsSingleSeason = async function (player, season) {
   if (data.stats) player.stats = data.stats[0].splits;
   else
     console.log(
-      `ERROR: There was an error getting stats for ${player.fullName}... RESPONSE MESSAGE: ${data.message}`
+      `[ERROR]: There was an error getting stats for '${player.fullName}'... [RESPONSE MESSAGE]: ${data.message}`
     );
   return player;
 };
 
-let players = await getPlayers();
-const playerPromises = players.map((player) =>
-  getPlayersStatsSingleSeason(player, '20202021')
+const players = await Promise.all(
+  (
+    await getPlayers()
+  ).map((player) => getPlayersStatsSingleSeason(player, '20202021'))
 );
-players = await Promise.all(playerPromises);
-// players.forEach((player) => {
-//   if (player.position === 'G') console.log(player);
-// });
-console.log(players[0]);
+
 console.log(players.length);
+console.log(players[0]);
+
+const mongoClient = new MongoClient(process.env.MONGO_CONNECTION, {
+  useUnifiedTopology: true,
+});
+await mongoClient.connect();
+const playersCollection = mongoClient.db('playerdb').collection('player-stats');
+
+for (const player of players) {
+  const query = { _id: player.id };
+  const update = { $set: player };
+  const options = { upsert: true };
+  await playersCollection.updateOne(query, update, options);
+}
+
+await mongoClient.close();
