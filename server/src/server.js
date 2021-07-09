23603f36031823d56db25a742fcb0d11a097abf3
Cal Trainor <cal16trainor@gmail.com>
2021-07-08 23:58:06 -0300

    Added mongo connection and have one endpoint set up to fetch data from db

--- server/src/server.js
+++ server/src/server.js
@@ -1,13 +1,27 @@
 import express from 'express';
 import * as dotenv from 'dotenv';
+import { MongoClient } from 'mongodb';
+
 dotenv.config();
 
 import { updateDb } from './updatedb.js';
 
 const app = express();
+const mongoClient = new MongoClient(process.env.MONGO_CONNECTION, {
+  useUnifiedTopology: true,
+});
+await mongoClient.connect();
+const playersCollection = mongoClient.db('playerdb').collection('player-stats');
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
+});
+
+app.get('/api/players', async (req, res) => {
+  const cursor = playersCollection.find({});
+  const players = await cursor.toArray();
+
+  res.send(players);
 });
 
 app.get('/api/updatedb', async (req, res) => {
