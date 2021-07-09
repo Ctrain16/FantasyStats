444aa4cd25ab7444b18d0a10936eac38ff8d9e8e
Cal Trainor <cal16trainor@gmail.com>
2021-07-08 23:59:01 -0300

    have simple home screen setup that is fetching data from server

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,6 +1,15 @@
 <template>
   <div class="home">
     <h1>Home Page</h1>
+    <p v-show="players.length === 0">Fetching players...</p>
+    <p v-if="players[0] === 'error'">Error fetching players</p>
+    <div id="player-chart" v-else>
+      <ul id="player-list">
+        <li v-for="(player, i) in players" :key="i">
+          {{ player.fullName }}
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
@@ -9,6 +18,19 @@
 // import HelloWorld from '@/components/HelloWorld.vue';
 
 export default {
-  name: 'Home'
+  name: 'Home',
+  data() {
+    return {
+      players: []
+    };
+  },
+  async mounted() {
+    try {
+      this.players = await (await fetch('api/players')).json();
+    } catch (error) {
+      this.players.push('error');
+      console.error(error);
+    }
+  }
 };
 </script>
