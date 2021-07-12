35c4b0d2742ef69746fb3fb1b3b16f1768b44e4b
Cal Trainor <cal16trainor@gmail.com>
2021-07-11 22:50:31 -0300

    unstyled player table and pagination system implemented

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -3,39 +3,75 @@
     <p v-show="players.length === 0">Fetching players...</p>
     <p v-if="players[0] === 'error'">Error fetching players</p>
     <div v-else-if="players.length > 0" id="player-chart">
-      <ul id="player-list">
-        <li v-for="(player, i) in playersOnPage" :key="i">
-          {{ player.fullName }}
-        </li>
-      </ul>
+      <table id="player-stats-table">
+        <thead>
+          <tr>
+            <th>Player</th>
+            <th v-for="(stat, i) in playerStatCategories" :key="i">
+              {{ stat }}
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="(player, i) in playersOnPage" :key="i">
+            <td>{{ player.fullName }}</td>
+            <td v-for="(stat, j) in playerStats(i)" :key="j">{{ stat }}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+    <div id="pagination">
+      <button
+        v-for="page in numberOfPages"
+        :key="page"
+        @click="this.currentPage = page + 1"
+      >
+        {{ page + 1 }}
+      </button>
     </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /srck
-// import HelloWorld from '@/components/HelloWorld.vue';
-
 export default {
   name: 'Home',
   data() {
     return {
       players: [],
-      page: 1,
+      currentPage: 1,
       playersPerPage: 50
     };
+  },
+  methods: {
+    playerStats(index) {
+      return this.players[index]._stats.slice(-1)[0].stat;
+    }
   },
   computed: {
     playersOnPage() {
       return this.players.slice(
-        (this.page - 1) * this.playersPerPage,
-        this.page * this.playersPerPage
+        (this.currentPage - 1) * this.playersPerPage,
+        this.currentPage * this.playersPerPage
+      );
+    },
+
+    playerStatCategories() {
+      const index = 0;
+      while (this.players[index].position === 'G') index++;
+      return Object.keys(this.players[index]._stats.slice(-1)[0].stat);
+    },
+
+    numberOfPages() {
+      return Array.from(
+        Array(Math.ceil(this.players.length / this.playersPerPage)).keys()
       );
     }
   },
   async mounted() {
     try {
       this.players = await (await fetch('api/players')).json();
+      const x = this.players[0]._stats;
+      console.log(x.slice(-1)[0]);
     } catch (error) {
       this.players.push('error');
       console.error(error);
