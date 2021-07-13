83588b0b74da5ad4d3bf7fa9354da3bf66d7cb22
Cal Trainor <cal16trainor@gmail.com>
2021-07-12 21:38:13 -0300

    Improved player data chart by adding sorting functionality and styling

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -6,28 +6,36 @@
       <table id="player-stats-table">
         <thead>
           <tr>
+            <th>#</th>
             <th>Player</th>
-            <th v-for="(stat, i) in playerStatCategories" :key="i">
+            <th
+              v-for="(stat, i) in playerStatCategories"
+              :key="i"
+              @click="selectSortColumn"
+            >
               {{ stat }}
             </th>
           </tr>
         </thead>
         <tbody>
           <tr v-for="(player, i) in playersOnPage" :key="i">
+            <td>{{ i + 1 }}</td>
             <td>{{ player.fullName }}</td>
             <td v-for="(stat, j) in playerStats(i)" :key="j">{{ stat }}</td>
           </tr>
         </tbody>
       </table>
     </div>
-    <div id="pagination">
-      <button
+    <div class="pagination">
+      <a
         v-for="page in numberOfPages"
         :key="page"
-        @click="this.currentPage = page + 1"
+        @click="currentPage = page + 1"
+        :class="currentPage === page + 1 ? 'page-button active' : 'page-button'"
+        href="#"
       >
         {{ page + 1 }}
-      </button>
+      </a>
     </div>
   </div>
 </template>
@@ -39,12 +47,48 @@ export default {
     return {
       players: [],
       currentPage: 1,
-      playersPerPage: 50
+      playersPerPage: 50,
+      sortColumn: '',
+      sortDescending: true,
+      lastSortColumnIndex: 0
     };
   },
   methods: {
     playerStats(index) {
-      return this.players[index]._stats.slice(-1)[0].stat;
+      return this.players[
+        index + (this.currentPage - 1) * this.playersPerPage
+      ]._stats.slice(-1)[0].stat;
+    },
+
+    selectSortColumn(e) {
+      const tableHeaders = document.getElementById('player-stats-table')
+        .children[0].children[0].children;
+      const sortColumnIndex = [...tableHeaders].findIndex(
+        header => header.textContent === e.target.textContent
+      );
+
+      [...document.getElementById('player-stats-table').rows].forEach(row => {
+        row.cells[this.lastSortColumnIndex].classList.remove('active-column');
+        const sortedColumn = row.cells[sortColumnIndex];
+        if (sortedColumn.nodeName === 'TD')
+          sortedColumn.classList.add('active-column');
+      });
+      this.lastSortColumnIndex = sortColumnIndex;
+
+      this.sortDescending =
+        this.sortColumn === e.target.textContent ? !this.sortDescending : true;
+      this.sortColumn = e.target.textContent;
+      this.currentPage = 1;
+      this.players = this.players.sort((p1, p2) => {
+        const p1stats = String(
+          p1._stats.slice(-1)[0].stat[this.sortColumn]
+        ).replace(':', '');
+        const p2stats = String(
+          p2._stats.slice(-1)[0].stat[this.sortColumn]
+        ).replace(':', '');
+
+        return this.sortDescending ? p2stats - p1stats : p1stats - p2stats;
+      });
     }
   },
   computed: {
@@ -81,5 +125,74 @@ export default {
 <style>
 .home {
   margin: 10px;
+}
+
+#player-chart {
+  overflow-x: auto;
+  margin-left: 10vw;
+  margin-right: 10vw;
+  box-shadow: 0px 4px 8px 0px rgb(182, 182, 182);
+}
+
+#player-stats-table {
+  font-family: Arial, Helvetica, sans-serif;
+  border-collapse: collapse;
+  width: 100%;
+  max-width: 80vw;
+}
+
+#player-stats-table td,
+#player-stats-table th {
+  border: 1px solid #ddd;
+  padding: 8px;
+  white-space: nowrap;
+}
+
+#player-stats-table tr:nth-child(even) {
+  background-color: #f2f2f2;
+}
+
+#player-stats-table tr:hover {
+  background-color: #ddd;
+}
+
+#player-stats-table th {
+  padding-top: 12px;
+  padding-bottom: 12px;
+  text-align: left;
+  background-color: var(--color-lightblue);
+  color: white;
+}
+
+.active-column {
+  background-color: var(--color-lightbluefaded);
+}
+
+.pagination {
+  display: flex;
+  justify-content: center;
+
+  min-width: fit-content;
+
+  padding-top: 2vh;
+  padding-bottom: 5vh;
+}
+
+.page-button {
+  color: var(--color-lightblue);
+  float: left;
+  padding: 4px 8px;
+  text-decoration: none;
+}
+
+.pagination a.active {
+  background-color: var(--color-lightblue);
+  color: white;
+  border-radius: 3px;
+}
+
+.pagination a:hover:not(.active) {
+  background-color: #ddd;
+  border-radius: 5px;
 }
 </style>
