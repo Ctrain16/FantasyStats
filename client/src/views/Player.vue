931d8aa4a317bcdfc91ea97ad2b6d4c8fe4787e6
Cal Trainor <cal16trainor@gmail.com>
2021-07-24 14:11:04 -0300

    Added 'Player page' table... and set width of chart class

--- client/src/views/Player.vue
+++ client/src/views/Player.vue
@@ -5,10 +5,26 @@
     <p><b>Position:</b> {{ player.position }}</p>
     <p><b>Number:</b> {{ player.number }}</p>
 
-    <table>
-      <thead></thead>
-      <tbody></tbody>
-    </table>
+    <div id="player-chart">
+      <table id="player-stats-table">
+        <thead>
+          <th>Season</th>
+          <th>Team</th>
+          <th v-for="(stat, i) in playerStatCategories" :key="i">
+            {{ stat }}
+          </th>
+        </thead>
+        <tbody>
+          <tr v-for="(season, i) in player._stats" :key="i">
+            <td>{{ season.season }}</td>
+            <td>{{ season.team.name }}</td>
+            <td v-for="(stat, i) in season.stat" :key="i">
+              {{ stat }}
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   </div>
   <div v-else>
     <p>Loading...</p>
@@ -26,6 +42,11 @@ export default {
       player: {}
     };
   },
+  computed: {
+    playerStatCategories() {
+      return Object.keys(this.player._stats.slice(-1)[0].stat);
+    }
+  },
   async mounted() {
     this.player = await (
       await fetch('api/player', {
@@ -42,4 +63,48 @@ export default {
 };
 </script>
 
-<style></style>
+<style>
+.player {
+  margin: 10px;
+}
+
+#player-chart {
+  overflow-x: auto;
+  width: fit-content;
+  box-shadow: 0px 4px 8px 0px rgb(182, 182, 182);
+  border: 1px solid var(--color-lightblue);
+}
+
+#player-stats-table {
+  border-collapse: collapse;
+  width: 100%;
+  max-width: 80vw;
+}
+
+#player-stats-table td,
+#player-stats-table th {
+  padding: 8px;
+  white-space: nowrap;
+}
+
+#player-stats-table td {
+  border: 1px solid #ddd;
+}
+
+#player-stats-table tr:nth-child(even) {
+  background-color: #f2f2f2;
+}
+
+#player-stats-table tr:hover {
+  background-color: #ddd;
+  cursor: pointer;
+}
+
+#player-stats-table th {
+  padding-top: 12px;
+  padding-bottom: 12px;
+  text-align: left;
+  background-color: var(--color-lightblue);
+  color: white;
+}
+</style>
