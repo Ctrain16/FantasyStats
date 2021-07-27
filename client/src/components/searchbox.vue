772bf0627982162cd54da5e949645e0b5f0c6a1e
Cal Trainor <cal16trainor@gmail.com>
2021-07-26 22:05:33 -0300

    search is implemented, with some things to still add such as suggestion nav and improved styling

--- client/src/components/searchbox.vue
+++ client/src/components/searchbox.vue
@@ -1,9 +1,21 @@
 <template>
-  <input
-    class="searchbox"
-    placeholder="Search for player..."
-    @input="searchForPlayer($event.target.value)"
-  />
+  <form class="search-suggestion" @submit="submit()">
+    <input
+      class="searchbox"
+      placeholder="Search for player..."
+      v-model="search"
+      @input="searchForPlayer()"
+    />
+    <div v-if="searchResults.length > 0" class="search-suggestion-items">
+      <div
+        v-for="(player, i) in searchResults"
+        :key="i"
+        @click="clickPlayer(player._id)"
+      >
+        <p>{{ player.fullName }}</p>
+      </div>
+    </div>
+  </form>
 </template>
 
 <script>
@@ -11,16 +23,31 @@ export default {
   name: 'SearchBox',
   data() {
     return {
-      players: []
+      players: [],
+      search: '',
+      searchResults: []
     };
   },
   methods: {
-    searchForPlayer(input) {
-      const results = this.players
-        .filter(player =>
-          player.fullName.toLowerCase().startsWith(input.toLowerCase())
-        )
-        .slice(0, 5);
+    searchForPlayer() {
+      this.searchResults = [];
+      if (this.search !== '') {
+        this.searchResults = this.players
+          .filter(player =>
+            player.fullName.toLowerCase().startsWith(this.search.toLowerCase())
+          )
+          .slice(0, 5);
+      }
+    },
+
+    clickPlayer(playerID) {
+      this.searchResults = [];
+      this.$router.push(`/player/${playerID}`);
+    },
+
+    submit() {
+      if (this.searchResults.length > 0)
+        this.clickPlayer(this.searchResults[0]._id);
     }
   },
   async mounted() {
@@ -37,6 +64,16 @@ export default {
 </script>
 
 <style>
+.search-suggestion {
+  /*the container must be positioned relative:*/
+  position: relative;
+  display: flex;
+  width: 100%;
+  height: 60px;
+  padding: 10px 25px;
+  max-width: 400px;
+}
+
 .searchbox {
   margin: 10px;
   padding-left: 20px;
@@ -47,5 +84,38 @@ export default {
   border: 1px solid var(--color-blue);
   border-radius: 5px;
   box-shadow: 0px 4px 4px 0px rgba(46, 46, 46, 0.26);
+}
+
+.search-suggestion-items {
+  position: absolute;
+  margin: 0 25px;
+
+  color: black;
+
+  z-index: 99;
+  top: 60px;
+  left: 0;
+  right: 0;
+}
+
+.search-suggestion-items div {
+  padding: 10px;
+  margin: 0px 10px 0px 10px;
+  cursor: pointer;
+
+  box-shadow: 0px 4px 4px 0px rgb(51, 51, 51);
+  background-color: #fff;
+
+  border: 1px solid black;
+  border-top: none;
+}
+
+.search-suggestion-items div:hover {
+  background-color: #e9e9e9;
+}
+
+.search-suggestion-active {
+  background-color: DodgerBlue !important;
+  color: #ffffff;
 }
 </style>
