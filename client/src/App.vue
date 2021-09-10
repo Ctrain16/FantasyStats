450a5e1e6a3db2985ddc0c20cfc0e642e2929d30
Cal Trainor <cal16trainor@gmail.com>
2021-09-10 20:26:00 -0300

    Added loading indicator on landing page

--- client/src/App.vue
+++ client/src/App.vue
@@ -2,7 +2,11 @@
   <div class="page-container">
     <div class="content-wrap">
       <Header></Header>
-      <router-view />
+      <router-view v-if="storeIntialized" />
+      <div v-else class="loading-section">
+        <p style="padding: 1rem">{{ loadingMessage }}</p>
+        <div class="loader"></div>
+      </div>
     </div>
     <Footer></Footer>
   </div>
@@ -17,6 +21,22 @@ export default {
   components: {
     Header,
     Footer
+  },
+  data() {
+    return {
+      storeIntialized: false,
+      loadingMessage: 'Loading.'
+    };
+  },
+  async mounted() {
+    let dotCount = 1;
+    const loadingInterval = setInterval(() => {
+      dotCount = dotCount === 3 ? 1 : dotCount + 1;
+      this.loadingMessage = 'Loading' + '.'.repeat(dotCount);
+    }, 250);
+    await this.$store.dispatch('initialize');
+    this.storeIntialized = true;
+    clearInterval(loadingInterval);
   }
 };
 </script>
@@ -56,5 +76,34 @@ export default {
 
 .content-wrap {
   padding-bottom: 7vh;
+}
+
+.loading-section {
+  position: absolute;
+  top: 40%;
+  left: 50%;
+  transform: translate(-50%, -60%);
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.loader {
+  border: 8px solid var(--color-ivory); /* Light grey */
+  border-top: 8px solid #3498db; /* Blue */
+  border-radius: 50%;
+  width: 30px;
+  height: 30px;
+  animation: spin 1s linear infinite;
+}
+
+@keyframes spin {
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
 }
 </style>
--- client/src/main.js
+++ client/src/main.js
@@ -3,9 +3,7 @@ import App from './App.vue';
 import router from './router';
 import store from './store';
 
-store.dispatch('initialize').then(() => {
-  createApp(App)
-    .use(store)
-    .use(router)
-    .mount('#app');
-});
+createApp(App)
+  .use(store)
+  .use(router)
+  .mount('#app');
